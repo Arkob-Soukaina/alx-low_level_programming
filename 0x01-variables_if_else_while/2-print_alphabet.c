6fc3a19b919ba69540528340fa91880f039c5540
Aniakous <soukainaark05@gmail.com>
2023-02-17 06:49:30 -0800

    Task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets.
+ * 
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
